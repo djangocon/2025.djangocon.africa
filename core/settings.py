b8719b237083fe1f5587ec9f248ef845b8f9862d
Anna Makarudze <amakarudze@gmail.com>
2024-06-17 08:59:26 +0200

    Fix ruff and coverage CI issues

--- core/settings.py
+++ core/settings.py
@@ -18,14 +18,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DJANGO_DEBUG")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET", "")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+SECRET_KEY = "djangocon" if DEBUG else os.environ.get("DJANGO_SECRET", "")
 
 ALLOWED_HOSTS = []
 
