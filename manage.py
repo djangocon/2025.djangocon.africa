5e1c70b288aaad2a390f6835689ac94d3bc8b28e
Anna Makarudze <amakarudze@gmail.com>
2024-06-17 09:03:59 +0200

    Fix ruff errors

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv(filename=".env"))
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
