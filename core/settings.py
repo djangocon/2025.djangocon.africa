e2a5d92904dc0b07a8683fe1f3198671f42db281
Anna Makarudze <amakarudze@gmail.com>
2024-06-17 09:09:06 +0200

    Fix Debug setting

--- core/settings.py
+++ core/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+DEBUG = os.environ.get("DJANGO_DEBUG") != False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "djangocon" if DEBUG else os.environ.get("DJANGO_SECRET", "")
+SECRET_KEY = "djangocon" if DEBUG else os.environ.get("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
