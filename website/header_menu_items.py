69b1fd4ce6f18eea266d9b13c8a1aac5c88c78a3
Sheena <sheena.oconnell@gmail.com>
2024-10-21 06:01:12 +0200

    ruff cleanup

--- website/header_menu_items.py
+++ website/header_menu_items.py
@@ -1,24 +1,22 @@
 from dataclasses import dataclass, field
-from typing import List 
+from typing import List
+
 # from django.urls import reverse
+
 
 @dataclass
 class HeaderLink:
     label: str
     href: str = ""
-    children: List["HeaderLink"] =field(default_factory=list)
+    children: List["HeaderLink"] = field(default_factory=list)
 
 
 header_menu_items = [
-  HeaderLink(
-      label="Home",
-      href="/"
-  ),
-  HeaderLink(
-      label="About",
-      children = [
-        #   HeaderLink("Team", href=reverse("page_team")),
-          
-      ]
-  ),
+    HeaderLink(label="Home", href="/"),
+    HeaderLink(
+        label="About",
+        children=[
+            #   HeaderLink("Team", href=reverse("page_team")),
+        ],
+    ),
 ]
