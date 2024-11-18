92d4487861da20154f2c5b2b870e6368d19cd7bf
Sheena <sheena.oconnell@gmail.com>
2024-11-18 09:16:18 +0200

    added allowed host

--- core/settings_prod.py
+++ core/settings_prod.py
@@ -3,7 +3,7 @@ from .settings_base import *
 import os
 
 
-ALLOWED_HOSTS = ["2025.djangocon.africa"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "2025.djangocon.africa"]
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
