1e1ed455bebc055e8bb8294f5e150adfd84d67ad
afenoum <anja1catus@gmail.com>
2024-11-19 20:21:30 +0300

    cleaning code

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
-
-from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView
-from django.contrib.messages.views import SuccessMessageMixin
 
 
 def register(request):
