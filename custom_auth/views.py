3b19f93f173f457fb9d66ae37b01804dad52912f
afenoum <anja1catus@gmail.com>
2024-11-19 20:12:34 +0300

    we don't need to rewrite the reset_password_view

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -12,17 +12,6 @@ from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'templates/registration/password_reset_form.html'
-    email_template_name = 'templates/registration/password_reset_email.html'
-    subject_template_name = 'templates/registration/password_reset_subject'
-    success_message = "We've emailed you instructions for setting your password, " \
-                      "if an account exists with the email you entered. You should receive them shortly." \
-                      " If you don't receive an email, " \
-                      "please make sure you've entered the address you registered with, and check your spam folder."
-    success_url = reverse_lazy('page_home')
-
-
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
