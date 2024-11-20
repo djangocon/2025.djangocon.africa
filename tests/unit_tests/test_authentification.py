e7d8b72bb4674a044a692b6c28016b5841b49090
afenoum <anja1catus@gmail.com>
2024-11-20 20:36:18 +0300

    register unit test

--- tests/unit_tests/test_authentification.py
+++ tests/unit_tests/test_authentification.py
@@ -14,7 +14,19 @@ class UserTestCase(TestCase):
         client = Client()
         u = User.objects.get(email="alice@wonderland.com")
         response = client.post("/accounts/login/",
-                               dict(email=u.email,password=u.password), content_type="application/json",)
+                               dict(email=u.email,password=u.password),
+                               content_type="application/json")
         assert response.status_code == 200
         assert u.is_authenticated == True
 
+    def test_register_new_member(self):
+        client = Client()
+        response = client.post("/accounts/register/",
+                               dict(email="rabbit.white@wonderland.com",
+                                    last_name="Rabbit", first_name="White",
+                                    password1="FNEYghfr",
+                                    password2="FNEYghfr"),
+                               format="text/html")
+        self.assertRedirects(response, "/accounts/login/", status_code=302,
+                             target_status_code=200,
+                             fetch_redirect_response=True)
