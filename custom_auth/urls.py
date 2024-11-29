b9ff79e4e50b34c9ac970fae53c93329588a78d9
afenoum <anja1catus@gmail.com>
2024-11-29 16:42:05 +0300

    cleaning code, fix test

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -2,6 +2,4 @@ from django.urls import path, include
 from custom_auth import views as custom_auth_views
 
 urlpatterns = [
-    path("accounts/register/", custom_auth_views.register, name ="register"),
-    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- tests/unit_tests/test_authentification.py
+++ tests/unit_tests/test_authentification.py
@@ -3,7 +3,6 @@ from django.core import mail
 from django.test import Client
 from pytest_django.asserts import assertRedirects, assertTemplateUsed
 from custom_auth.models import User
-from custom_auth import utils
 
 client = Client()
 
@@ -17,12 +16,6 @@ def user_fixture(db, django_user_model):
     )
     return default_user
 
-def test_send_registration_email():
-   utils.send_registration_email(
-       email="alice@wonderland.com",
-       username ="alice")
-   assert len(mail.outbox) == 1
-
 @pytest.mark.django_db
 def test_register():
     response = client.post(
@@ -32,35 +25,32 @@ def test_register():
         password1="FNEYghfr",
         password2="FNEYghfr"),
         format="text/html")
-    assertRedirects(response, "/accounts/login/", status_code=302,
-                             target_status_code=200,
-                             fetch_redirect_response=True)
-    assert len(mail.outbox) == 1
+    assert response.status_code == 404
 
 
 @pytest.mark.django_db
 def test_get_register_page():
-    response = client.get("/accounts/register/")
+    response = client.get("/accounts/signup/")
     assert response.status_code ==  200
-    assertTemplateUsed(response, "registration/register.html")
+    assertTemplateUsed(response, "account/signup.html")
 
 @pytest.mark.django_db
 def test_valid_template_for_forgotten_password():
-    response = client.get("/accounts/password_reset/")
-    assertTemplateUsed(response, "registration/password_reset_form.html")
+    response = client.get("/accounts/password/reset/")
+    assertTemplateUsed(response, "account/password_reset.html")
 
 
 @pytest.mark.django_db
 def test_valid_email_sending_for_forgotten_password(user_fixture):
     u = user_fixture
     response = client.post(
-        "/accounts/password_reset/",
+        "/accounts/password/reset/",
         dict(email=u.email),
         format="text/html"
     )
     assertRedirects(
         response,
-        "/accounts/password_reset/done/",
+        "/accounts/password/reset/done/",
         status_code=302,
         target_status_code=200,
         fetch_redirect_response=True
@@ -72,15 +62,13 @@ def test_valid_email_sending_for_forgotten_password(user_fixture):
 
 def test_valid_url_to_reset_password(user_fixture):
     u = User.objects.get(email = "alice@wonderland.com")
-    client.post("/accounts/password_reset/",
+    client.post("/accounts/password/reset/",
                                 dict(email=u.email),
                                format="text/html")
     email_lines = mail.outbox[0].body.splitlines()
-    url = email_lines[5]
-    response = client.get(url)
-    assert response.status_code == 302
+    url = email_lines[6]
     re = client.post(
-        response.url,
+        url,
         dict(password1="FNEYghfr",password2="FNEYghfr"),
         format="text/html")
     assert re.status_code == 200
