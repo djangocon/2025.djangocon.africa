1221b882b2ed237fed81fa7d5679148258577956
afenoum <anja1catus@gmail.com>
2024-11-30 07:51:36 +0300

    .

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,20 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.template import Context
 
-from .forms import UserRegisterForm
-from  .utils import send_registration_email
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            send_registration_email(email, username)
-            messages.success(request, f'Your account has been created ! You are now able to log in')
-            return redirect('account_login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'registration/register.html', {'form': form, 'title':'register here'})
+
