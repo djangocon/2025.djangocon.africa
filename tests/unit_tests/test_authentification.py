8dc4b897f5e47a12da68cea7fb7daf9d66a034b8
afenoum <anja1catus@gmail.com>
2024-12-16 12:05:52 +0300

    review test

--- tests/unit_tests/test_authentification.py
+++ tests/unit_tests/test_authentification.py
@@ -1,51 +1,44 @@
 import pytest
 from django.core import mail
-from django.test import Client
 from pytest_django.asserts import assertRedirects, assertTemplateUsed
-from custom_auth.models import User
+from tests.conftest import user
 
-client = Client()
-
-@pytest.fixture
-def user_fixture(db, django_user_model):
-    default_user = django_user_model.objects.create(
-        first_name="Alice",
-        last_name="Nolastname",
-        email="alice@wonderland.com",
-        password="nfpt30x49q2obt9"
-    )
-    return default_user
 
 @pytest.mark.django_db
-def test_register():
+def test_register(client):
     response = client.post(
-        "/accounts/register/",
+        "/accounts/signup/",
         dict(email="rabbit.white@wonderland.com",
         last_name="Rabbit", first_name="White",
         password1="FNEYghfr",
         password2="FNEYghfr"),
         format="text/html")
-    assert response.status_code == 404
+    assertRedirects(
+        response,
+        "/accounts/confirm-email/",
+        status_code=302,
+        target_status_code=200,
+        fetch_redirect_response=True
+    )
 
 
 @pytest.mark.django_db
-def test_get_register_page():
+def test_get_register_page(client):
     response = client.get("/accounts/signup/")
     assert response.status_code ==  200
     assertTemplateUsed(response, "account/signup.html")
 
 @pytest.mark.django_db
-def test_valid_template_for_forgotten_password():
+def test_valid_template_for_forgotten_password(client):
     response = client.get("/accounts/password/reset/")
     assertTemplateUsed(response, "account/password_reset.html")
 
 
 @pytest.mark.django_db
-def test_valid_email_sending_for_forgotten_password(user_fixture):
-    u = user_fixture
+def test_valid_email_sending_for_forgotten_password(user, client):
     response = client.post(
         "/accounts/password/reset/",
-        dict(email=u.email),
+        dict(email=user.email),
         format="text/html"
     )
     assertRedirects(
@@ -55,15 +48,14 @@ def test_valid_email_sending_for_forgotten_password(user_fixture):
         target_status_code=200,
         fetch_redirect_response=True
     )
-    assert mail.outbox[0].to == [u.email]
+    assert mail.outbox[0].to == [user.email]
     assert len(mail.outbox) ==  1
 
 
 
-def test_valid_url_to_reset_password(user_fixture):
-    u = User.objects.get(email = "alice@wonderland.com")
+def test_valid_url_to_reset_password(user, client):
     client.post("/accounts/password/reset/",
-                                dict(email=u.email),
+                                dict(email=user.email),
                                format="text/html")
     email_lines = mail.outbox[0].body.splitlines()
     url = email_lines[6]
