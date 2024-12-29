c746bdc66d9cd554715b047352fe7dc86feb41ce
Anna Makarudze <anna@makarudze.com>
2024-12-29 21:09:14 +0100

    Fix conflicts

--- core/settings_base.py
+++ core/settings_base.py
@@ -14,12 +14,6 @@ import os
 from pathlib import Path
 
 
-def gettext(s):
-    """
-    i18n passthrough
-    """
-    return s
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
