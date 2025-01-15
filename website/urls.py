0b3f5d6b982c804e9e98dbf9aab487685e68006f
Derek <reign.derek@yahoo.com>
2025-01-15 22:14:33 +0000

    Add news detail page

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.page_home, name="page_home"),
     path("coc", views.page_coc, name="page_coc"),
+    path("news/<slug:slug>/", views.page_news_detail, name="page_news_detail"),
 ]
--- website/views.py
+++ website/views.py
@@ -7,3 +7,7 @@ def page_home(request):
 
 def page_coc(request):
     return render(request, "page_coc.html", {"is_white_header": True})
+
+
+def page_news_detail(request, slug):
+    return render(request, "page_news_detail.html", {"is_white_header": True})
