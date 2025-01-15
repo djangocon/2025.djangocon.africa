427df369bb61fb392b2eb3d5a4841421bbd4873f
Derek <reign.derek@yahoo.com>
2025-01-15 22:13:56 +0000

    Add `coc` page

--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def page_home(request):
     return render(request, "page_home.html")
+
+
+def page_coc(request):
+    return render(request, "page_coc.html", {"is_white_header": True})
