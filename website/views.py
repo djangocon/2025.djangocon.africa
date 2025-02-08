82c6047e02528a120f3755420c0476ce3820dc40
Derek <reign.derek@yahoo.com>
2025-02-08 15:09:57 +0000

    Make white header default

--- website/views.py
+++ website/views.py
@@ -5,23 +5,23 @@ from datetime import datetime
 
 
 def page_home(request):
-    return render(request, "page_home.html")
+    return render(request, "page_home.html", {"is_white_header": False})
 
 
 def page_coc(request):
-    return render(request, "page_coc.html", {"is_white_header": True})
+    return render(request, "page_coc.html")
 
 
 def page_news_detail(request, slug):
-    return render(request, "page_news_detail.html", {"is_white_header": True})
+    return render(request, "page_news_detail.html")
 
 
 def page_speaking(request):
-    return render(request, "page_speaking.html", {"is_white_header": True})
+    return render(request, "page_speaking.html")
 
 
 def page_speaker_resources(request):
-    return render(request, "page_speaker_resources.html", {"is_white_header": True})
+    return render(request, "page_speaker_resources.html")
 
 
 def page_sponsor_us(request):
