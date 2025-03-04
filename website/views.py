f123a131a50172d80ef19f7dd44da0293f75c42c
Derek <reign.derek@yahoo.com>
2025-03-04 22:48:36 +0000

    Add schedule

--- website/views.py
+++ website/views.py
@@ -5,7 +5,39 @@ from datetime import datetime
 
 
 def page_home(request):
-    return render(request, "page_home.html", {"is_white_header": False})
+    schedule = [
+        [
+            [('Jan 3rd', 'Announce Conference', True)],
+            [('Jul 7th', 'Conference Schedule is announced', False)],
+        ],
+        [
+            [('Feb 1st', 'Call For Proposals open', True),
+             ('Feb 14th', 'Opportunity Grant opens', False)],
+            [('Aug 8th', 'Django Girls starts', False),
+             ('Aug 9th', 'Django Girls ends', False)],
+        ],
+        [
+            [('Mar 1st', 'Ticket sales open', False),
+             ('Mar 31st', 'Call For Proposals close', False)],
+            [('Aug 11th', 'Conference starts', False),
+             ('Aug 15th', 'Conference ends', False)],
+        ],
+        [
+            [('Apr 1st', 'Opportunity Grants notifications', False),
+             ('Apr 1st', 'Call For Proposals notifications', False)],
+            [('Aug 13th', 'UbuCon starts', False),
+             ('Aug 15th', 'UbuCon ends', False)],
+        ],
+    ]
+
+    return render(
+        request,
+        "page_home.html",
+        {
+            "is_white_header": False,
+            "schedule": schedule
+        }
+    )
 
 
 def page_coc(request):
@@ -34,7 +66,6 @@ def opportunity_grants(request):
         request,
         "page_opportunity_grants.html",
         {
-            "is_white_header": True,
             "og_state": og_state,
             "og_is_before": og_state.closing_date > datetime.now(og_state.closing_date.tzinfo) if og_state else None,
         }
