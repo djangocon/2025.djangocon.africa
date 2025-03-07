9f4157604378c15af7b36d41aa88e2d074f05489
theShinigami <salimabdellanuru@gmail.com>
2025-03-07 23:36:33 +0300

    refactor: make the cms path fetch from env on prod

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include, re_path
+from django.conf import settings
 
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path("cms/", include(wagtailadmin_urls)), # TODO: make the admin fetch from the env
     path("documents/", include(wagtaildocs_urls)),
     re_path(r'', include(wagtail_urls)),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns.append(path("cms/", include(wagtailadmin_urls)))
+else:
+    urlpatterns.append(path(settings.CMS_PATH, include(wagtailadmin_urls)))
 
