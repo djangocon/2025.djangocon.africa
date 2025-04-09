0832bc62d069b62326edaf2110e586fee792501f
theShinigami <salimabdellanuru@gmail.com>
2025-04-09 18:58:50 +0300

    refactor: move Schedules list to constant and corrected schedule date

--- website/constants.py
+++ website/constants.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 SOCIAL_MEDIA_LINKS = {
     "Twitter/X": "https://twitter.com/djcafrica",
     "LinkedIn": "https://linkedin.com/company/djcafrica",
@@ -6,3 +8,33 @@ SOCIAL_MEDIA_LINKS = {
     "Masterdon": "https://fosstodon.org/@djangoconafrica/111065411415401242",
     "Bluesky": "https://bsky.app/profile/djcafrica.bsky.social",
 }
+
+SCHEDULES = [
+    [(datetime(2025, 1, 23), 'Announce Conference')],
+    [
+        (datetime(2025, 2, 3), 'Call For Proposals Open'),
+        (datetime(2025, 2, 12), 'Call for Sponsors Open'),
+        (datetime(2025, 2, 14), 'Opportunity Grant Open')
+    ],
+    [
+        (datetime(2025, 3, 1), 'Ticket Sales Open'),
+        (datetime(2025, 3, 31), 'Opportunity Grants && Call For Proposals close'),
+    ],
+    [
+        (datetime(2025, 4, 8), 'Talk review start'),
+        (datetime(2025, 4, 28), 'Opportunity Grant && Call for Proposal Notifications')
+    ],
+    [
+        (datetime(2025, 8, 8), 'Django Girls starts'),
+        (datetime(2025, 8, 9), 'Django Girls ends')
+    ],
+    [
+        (datetime(2025, 8, 11), 'Conference starts'),
+        (datetime(2025, 8, 13), 'UbuCon starts')
+    ],
+    [
+        (datetime(2025, 8, 15), 'Conference ends'),
+        (datetime(2025, 8, 15), 'UbuCon ends')
+    ]
+]
+
--- website/views.py
+++ website/views.py
@@ -3,31 +3,17 @@ from website.models import OpportunityGrants
 from django.conf import settings
 from datetime import datetime
 
+from website.constants import SCHEDULES
+
 
 def page_home(request):
-    schedule = [
-        [(datetime(2025, 1, 3), 'Announce Conference')],
-        [(datetime(2025, 2, 1), 'Call For Proposals open'),
-         (datetime(2025, 2, 14), 'Opportunity Grant opens')],
-        [(datetime(2025, 3, 1), 'Ticket sales open'),
-         (datetime(2025, 3, 31), 'Call For Proposals close')],
-        [(datetime(2025, 4, 1), 'Opportunity Grants notifications'),
-         (datetime(2025, 4, 1), 'Call For Proposals notifications')],
-        [(datetime(2025, 7, 7), 'Conference Schedule is announced')],
-        [(datetime(2025, 8, 8), 'Django Girls starts'),
-         (datetime(2025, 8, 9), 'Django Girls ends')],
-        [(datetime(2025, 8, 11), 'Conference starts'),
-         (datetime(2025, 8, 13), 'UbuCon starts')],
-        [(datetime(2025, 8, 15), 'Conference ends'),
-         (datetime(2025, 8, 15), 'UbuCon ends')]
-    ]
 
     return render(
         request,
         "page_home.html",
         {
             "is_white_header": False,
-            "schedule": schedule
+            "schedule": SCHEDULES
         }
     )
 
