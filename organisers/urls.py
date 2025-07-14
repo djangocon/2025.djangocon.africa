19f835c95b0bb8a23aba9ae6c9ac4bdde935fdbd
theShinigami <salimabdellanuru@gmail.com>
2025-07-14 17:40:04 +0300

    refactor: not needed since there are a few organisers, inserting data manually

--- organisers/urls.py
+++ organisers/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
-from .views import (
-    OrganisersListView,
-    OrganiserSubmissionView,
-    OrganiserSubmissionSuccessView,
-)
+from .views import OrganisersListView
 
 urlpatterns = [
     path("", OrganisersListView.as_view(), name="organisers"),
-    path("submit/", OrganiserSubmissionView.as_view(), name="organiser_submission"),
-    path(
-        "submit/success/",
-        OrganiserSubmissionSuccessView.as_view(),
-        name="organiser_submission_success",
-    ),
 ]
--- organisers/views.py
+++ organisers/views.py
@@ -1,10 +1,5 @@
-from django.views.generic import ListView, CreateView
-from django.contrib import messages
-from django.urls import reverse_lazy
+from django.views.generic import ListView
 from .models import Organiser
-from .forms import OrganiserSubmissionForm
-
-# Create your views here.
 
 
 class OrganisersListView(ListView):
@@ -12,30 +7,3 @@ class OrganisersListView(ListView):
     template_name = "organisers/organisers.html"
     context_object_name = "organisers"
     queryset = Organiser.objects.filter(approved=True)
-
-
-class OrganiserSubmissionView(CreateView):
-    model = Organiser
-    form_class = OrganiserSubmissionForm
-    template_name = "organisers/submit_organiser.html"
-    success_url = reverse_lazy("organiser_submission_success")
-
-    def form_valid(self, form):
-        # Set approved to False by default
-        form.instance.approved = False
-        messages.success(
-            self.request,
-            "Thank you for submitting your organiser information! "
-            "Your submission is pending approval and will be displayed once reviewed.",
-        )
-        return super().form_valid(form)
-
-
-class OrganiserSubmissionSuccessView(ListView):
-    """Simple success page after submission"""
-
-    model = Organiser
-    template_name = "organisers/submission_success.html"
-
-    def get_queryset(self):
-        return Organiser.objects.none()  # We don't need any data for success page
